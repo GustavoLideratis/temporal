b4ecf18560fef6f5a3849e92d34c5925d35bda38
Gustavo Anchahua Taype <gustavo@MacBook-Air-de-Gustavo.local>
2017-01-27 09:32:35 -0500

    Comentarios Product View

--- ProductView.js
+++ ProductView.js
@@ -5,69 +5,75 @@ $(window).load(function() {
 
     // Esta funcion recorrera el objeto dataLayer, buscando el dataLayer.pageCategory:  "Product".
     var posicion = LideratisHelper.LocationIndexVTEX(dataLayer, 'Product', 0);
-    // Cumple con la condicion, exite el dataLayer.pageCategory: "Product".
-    //if (posicion > -1) {
+    // Si cumple con la condicion, exite el dataLayer.pageCategory: "Product".
+    if (posicion > -1) {
+        //Variable de atributos de un producto.
+        var precioRegular = LideratisHelper.NumberOfDecimal(dataLayer[posicion].productListPriceTo, 2);
+        var precioVenta = LideratisHelper.NumberOfDecimal(dataLayer[posicion].productPriceTo, 2);
+        var porcentaje = $(".flags p:eq(0)").html();
 
-    var precioRegular = LideratisHelper.tempNumberOfDecimal(dataLayer[0].productListPriceTo, 2);
-    var precioVenta = LideratisHelper.tempNumberOfDecimal(dataLayer[0].productPriceTo, 2);
-    var porcentaje = $(".flags p:eq(0)").html();
+        // Posicion de Atributos
+        var vExploreAttributesPRO = new Array();
+        /* Atributos
+        Attr1 : Marca de Productos
+        Attr3 : Precio Regular
+        Attr4 : Precio de Venta
+        Attr5 : Poncentaje de Descuento
+        Attr6 : Flag de Atiquetas de la vista de un Producto
+        */
+        vExploreAttributesPRO[1] = dataLayer[posicion].productBrandName;
+        vExploreAttributesPRO[3] = precioRegular;
+        vExploreAttributesPRO[4] = precioVenta;
+        vExploreAttributesPRO[5] = porcentaje;
 
-    // Posicion de Atributos
-    var vExploreAttributesPRO = new Array();
+        var listaFlag = new Array();
+        $(".flags p").each(function(e) {
+            var flaArray = $(this).attr('class').split('flag ')[1] + "_" + $(this).html();
+            listaFlag.push(flaArray);
+        });
+        var flagJoin = listaFlag.join('|');
+        if (flagJoin != '') {
+            vExploreAttributesPRO[6] = flagJoin; // Attr6 : Flag de Atiquetas de la vista de un Producto
+        }
 
-    vExploreAttributesPRO[1] = dataLayer[0].productBrandName;
-    vExploreAttributesPRO[3] = precioRegular;
-    vExploreAttributesPRO[4] = precioVenta;
-    vExploreAttributesPRO[5] = porcentaje;
+        /* Declaracion de variables para almacenar datos de producto - Tag ProductView*/
+        var vProductID = vtxctx.skus;
+        var vProductName = dataLayer[posicion].productName;
+        var vPrimaryCategory = dataLayer[posicion].productCategoryId;
+        var vPrice = LideratisHelper.NumberOfDecimal(dataLayer[posicion].productPriceTo, 2);
 
-    var listaFlag = new Array();
-    $(".flags p").each(function(e) {
-        var flaArray = $(this).attr('class').split('flag ')[1] + "_" + $(this).html();
-        listaFlag.push(flaArray);
-    });
-    //console.log("FLAG JOIN 1 " + listaFlag);
-    var flagJoin = listaFlag.join('|');
-    //console.log("FLAG JOIN 2 " + listaFlag);
-    if (flagJoin != '') {
-        vExploreAttributesPRO[6] = flagJoin;
+        var dataProductView = new Array();
+        // Datos del Tag Productview.
+        dataProductView['productID'] = vProductID;
+        dataProductView['productName'] = vProductName;
+        dataProductView['primaryCategory'] = vPrimaryCategory;
+        dataProductView['exploreAttributes'] = LideratisHelper.GetArrayAttributes(vExploreAttributesPRO);
+        dataProductView['virtualCategory'] = null;
+        var tagProductView = new LideratisLibrary.ProductView(dataProductView);
+        // Almacenamos un array en el objeto digitalData.product
+        digitalData.product = [tagProductView.product[0]];
+        //Llamamos a la funcion del Tag del Producto  - window.tagProductView()
+        window.tagProductView();
+
+        /********************************************** Agregar Producto - Shopaction5 ***************************************************/
+
+        // variable que almacena la ubicacion de la etiqueta del botón dentro del html.
+        var vNomBtnAdd = '#BuyButton .buy-button';
+        $(document).on("click", vNomBtnAdd, function() {
+
+            var dataShopAction5 = new Array();
+            //Datos del Tag Shopaction5.
+            dataShopAction5['productID'] = vProductID;
+            dataShopAction5['productName'] = vProductName;
+            dataShopAction5['quantity'] = '1';
+            dataShopAction5['price'] = vPrice;
+            dataShopAction5['primaryCategory'] = vPrimaryCategory;
+            dataShopAction5['exploreAttributes'] = LideratisHelper.GetArrayAttributes(vExploreAttributesPRO);
+            digitalData = new LideratisLibrary.ShopAction5_BtnClick(dataShopAction5);
+            console.log(digitalData);
+            //Llamamos a la funcion del Tag del Shopaction5  - window.agregarCarrito()
+            window.agregarCarrito();
+        });
+
     }
-    //console.log("FLAG JOIN 3 " + listaFlag);
-
-    /* Declaracion de variables para almacenar datos de producto - ProductView*/
-    var vProductID = vtxctx.skus;
-    var vProductName = dataLayer[0].productName;
-    var vPrimaryCategory = dataLayer[0].productCategoryId;
-    var vPrice = LideratisHelper.tempNumberOfDecimal(dataLayer[0].productPriceTo, 2);
-
-    var dataProductView = new Array();
-    // Datos del Tag Productview.
-    dataProductView['productID'] = vProductID;
-    dataProductView['productName'] = vProductName;
-    dataProductView['primaryCategory'] = vPrimaryCategory;
-    dataProductView['exploreAttributes'] = LideratisHelper.GetArrayAttributes(vExploreAttributesPRO);
-    dataProductView['virtualCategory'] = null;
-    var tagProductView = new LideratisLibrary.ProductView(dataProductView);
-    // Almacenamos un array
-    digitalData.product = [tagProductView.product[0]];
-    window.tagProductView();
-    //}
-
-    /********************************************** Agregar Producto - Shopaction5 ***************************************************/
-
-    // variable que almacena la ubicacion de la etiqueta del botón dentro del html.
-    var vNomBtnAdd = '#BuyButton .buy-button';
-    $(document).on("click", vNomBtnAdd, function() {
-
-        var dataShopAction5 = new Array();
-        dataShopAction5['productID'] = vProductID;
-        dataShopAction5['productName'] = vProductName;
-        dataShopAction5['quantity'] = '1';
-        dataShopAction5['price'] = vPrice;
-        dataShopAction5['primaryCategory'] = vPrimaryCategory;
-        dataShopAction5['exploreAttributes'] = LideratisHelper.GetArrayAttributes(vExploreAttributesPRO);
-        digitalData = new LideratisLibrary.ShopAction5_BtnClick(dataShopAction5);
-        console.log(digitalData);
-        agregarCarrito();
-    });
-
 });
--- buscador.js
+++ buscador.js
@@ -3,62 +3,101 @@ $(window).load(function() {
 
     var vExploreAttributesBusq = new Array();
 
-    var posicionInt           = LideratisHelper.LocationIndexVTEX(dataLayer, 'InternalSiteSearch', 0);
-    var posicionCat           = LideratisHelper.LocationIndexVTEX(dataLayer, 'Category', 0);
+    var posicionInt = LideratisHelper.LocationIndexVTEX(dataLayer, 'InternalSiteSearch', 0);
+    var posicionCat = LideratisHelper.LocationIndexVTEX(dataLayer, 'Category', 0);
 
 
-    var vPageId               = "";
-    var vPrimaryCat           = "";
-    var vOnsiteSearchTerm     = "";
-    var vOnsiteSearchResults  = -1;
+    var vPageId = "";
+    var vPrimaryCat = "";
+    var vOnsiteSearchTerm = "";
+    var vOnsiteSearchResults = -1;
 
-    if(posicionInt != -1 && posicionCat == -1){
-      vOnsiteSearchResults = dataLayer[posicionInt].siteSearchResults;
+    if (posicionInt != -1 && posicionCat == -1) {
+        vOnsiteSearchResults = dataLayer[posicionInt].siteSearchResults;
 
-      if (vOnsiteSearchResults > 0)
-          vPageId = window.BUSCADORCRE;
-      else
-          vPageId = window.BUSCADORSRE;
+        if (vOnsiteSearchResults > 0)
+            vPageId = window.BUSCADORCRE;
+        else
+            vPageId = window.BUSCADORSRE;
 
-      var verificaCat   = typeof(dataLayer[posicionInt].siteSearchCategory);
-      var cantSub       = $("#mainCol .sub").length - 1;
-      var cbOrdenar     = (($("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html() == 'Seleccione') ? '' : $("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html());
+        var verificaCat = typeof(dataLayer[posicionInt].siteSearchCategory);
+        var cantSub = $("#mainCol .sub").length - 1;
+        //comentar sobre los supuestos por que se coloca el primer div
+        var cbOrdenar = (($("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html() == 'Seleccione') ? '' : $("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html());
 
-      vPrimaryCategory  = ((verificaCat === 'undefined') ? 'Busqueda' : dataLayer[posicionInt].siteSearchCategory);
-      vOnsiteSearchTerm = dataLayer[posicionInt].siteSearchTerm;
+        vPrimaryCategory = ((verificaCat === 'undefined') ? 'Busqueda' : dataLayer[posicionInt].siteSearchCategory);
+        vOnsiteSearchTerm = dataLayer[posicionInt].siteSearchTerm;
 
-      if (cbOrdenar != '')
-          vExploreAttributesBusq[1] = cbOrdenar;
+        if (cbOrdenar != '')
+            vExploreAttributesBusq[1] = cbOrdenar;
 
-    }else if(posicionInt == -1 && posicionCat != -1){
-      vOnsiteSearchResults = $('.wrapper').length;
-      vOnsiteSearchTerm    = $('.bread-crumb ul').children('.last').find('a:eq(0)').html();
+    } else if (posicionInt == -1 && posicionCat != -1) {
+        vOnsiteSearchResults = $('.wrapper').length;
+        vOnsiteSearchTerm = $('.bread-crumb ul').children('.last').find('a:eq(0)').html();
 
-      if (vOnsiteSearchResults > 0)
-          vPageId = window.BUSCADORCRE;
-      else
-          vPageId = window.BUSCADORSRE;
+        if (vOnsiteSearchResults > 0)
+            vPageId = window.BUSCADORCRE;
+        else
+            vPageId = window.BUSCADORSRE;
 
-      vPrimaryCategory = "Busqueda";
+        vPrimaryCategory = "Busqueda";
 
-      var cantSub = $("#mainCol .sub").length - 1;
-      var cbOrdenar = (($("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html() == 'Seleccione') ? '' : $("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html());
+        var cantSub = $("#mainCol .sub").length - 1;
+        var cbOrdenar = (($("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html() == 'Seleccione') ? '' : $("#mainCol .sub:eq(" + cantSub + ") select#O option:selected").html());
 
-      if (cbOrdenar != '')
-          vExploreAttributesBusq[1] = cbOrdenar;
+        if (cbOrdenar != '')
+            //verificar este atributo
+            vExploreAttributesBusq[1] = cbOrdenar;
 
     }
 
     var dataPageView = new Array();
-    vPageId += vPageId+window.SEPARADOR+window.SUFIJO
+    vPageId += vPageId + window.SEPARADOR + window.SUFIJO
 
-    dataPageView['pageId']              = vPageId;
-    dataPageView['primaryCategory']     = vPrimaryCategory;
-    dataPageView['onsiteSearchTerm']    = vOnsiteSearchTerm;
+    dataPageView['pageId'] = vPageId;
+    dataPageView['primaryCategory'] = vPrimaryCategory;
+    dataPageView['onsiteSearchTerm'] = vOnsiteSearchTerm;
     dataPageView['onsiteSearchResults'] = vOnsiteSearchResults;
-    dataPageView['exploreAttributes']   = LideratisHelper.GetArrayAttributes(vExploreAttributesBusq);
+    dataPageView['exploreAttributes'] = LideratisHelper.GetArrayAttributes(vExploreAttributesBusq);
 
     window.digitalData = new LideratisLibrary.PageView(dataPageView);
     window.functionLoad();
 
 });
+
+//Documentacion del todos los codigos
+
+
+// 1.
+// cuando seleccionamoos algun selector del combo - cuando se realiza una busqueda.
+// PageId => Busqueda con resultados - Filtro aplicados
+// Busqueda con resultados - Ordenamiento aplicado
+// atrinbuto 30  - Termino de Busqueda
+// atributo 31   - Cantidad de  resultados
+// atributo 32   - Filtro aplicados
+// atributo 33   - Selector del Combo  "Orden Seleccionado"
+
+// 2 Cuando realizamos un click en los enlaces de la izquierda, despues de realizar una busqueda.
+// PageID => Busqueda con resultados - Filtro aplicados
+// filtros facetados
+// se debe de realizar los mismos pasos anteriores, desglozando los atributos del 30 al 33
+// atrinbuto 30  - Termino de Busqueda
+// atributo 31   - Cantidad de  resultados
+// atributo 32   - Filtro aplicados
+// atributo 33   - Selector del Combo  "Orden Seleccionado".
+
+// verificar si se puede realizar un conversion event al seleccionar un valor del select menor precio, mejor evaluado.
+
+
+
+
+
+
+//el dni o email puede ser la llave.
+//para la mayoria de retail el login debe de ser el email
+
+
+
+/* Url donde se encuentran la cantidad larga de productos para realizar pruebas
+http://tienda.wong.com.pe/electro/pequenos-electrodomesticos
+*/
